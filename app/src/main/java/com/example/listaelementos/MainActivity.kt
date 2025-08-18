e7a7189d3382c5bc28d3afb541709a5f637a64b5
vinicarpes <vinicdf000@gmail.com>
2025-08-18 12:37:05 -0300

    feat: add item in list

--- app/src/main/java/com/example/listaelementos/MainActivity.kt
+++ app/src/main/java/com/example/listaelementos/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.example.listaelementos
 
-import android.app.Activity
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
-import java.util.concurrent.Executor
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // definindo o layout dessa activity ao criar o componente
         setContentView(R.layout.activity_main)
-        val spn_sexo = findViewById<Spinner>(R.id.spn_sexo)
-        val txt_idade = findViewById<EditText>(R.id.txt_idade)
-        val txt_resultado = findViewById<TextView>(R.id.txt_resultado)
-        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
-        spn_sexo.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listOf("Masculino", "Feminino"))
-
-        btn_calcular.setOnClickListener{
-            val sexo = spn_sexo.selectedItem as String
-            val idade = txt_idade.text.toString().toInt()
-            var resultado = 0
-            if(sexo == "Masculino"){
-                resultado = 65 - idade
-            }else{
-                resultado = 60 - idade
+        val txt_produto = findViewById<EditText>(R.id.txt_produto)
+        val list_view_produtos = findViewById<ListView>(R.id.list_view_produtos)
+        val btn_inserir = findViewById<Button>(R.id.btn_inserir)
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        btn_inserir.setOnClickListener {
+            val produto = txt_produto.text.toString()
+            if (produto.isNotBlank()) {
+                produtosAdapter.add(produto)
             }
-
-            txt_resultado.text = "Faltam $resultado anos para você se aposentar."
         }
+
+        list_view_produtos.adapter = produtosAdapter
     }
+
 }
