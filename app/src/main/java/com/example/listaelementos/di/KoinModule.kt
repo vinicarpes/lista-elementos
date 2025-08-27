1f884fe238809d92ad4d50d5adf8cad6521e7cb9
vinicarpes <vinicdf000@gmail.com>
2025-08-27 17:54:22 -0300

    refactor: apply DI for database, repository and MainViewModel
    issue-link: https://deliverymuchbrasil.atlassian.net/browse/CLIENTE-4550?atlOrigin=eyJpIjoiZTFiN2JhMzZlZjg0NDc1ZDk5YjU1NWJlYWNjN2M1YjciLCJwIjoiaiJ9
    issue-id: CLIENTE-4550
    issue-description: applied dependency injection for ProdutoRepository, Database instance and MainViewModel. Also created Application class to start koin

--- app/src/main/java/com/example/listaelementos/di/KoinModule.kt
+++ app/src/main/java/com/example/listaelementos/di/KoinModule.kt
@@ -3,16 +3,19 @@ package com.example.listaelementos.di
 import androidx.room.Room
 import com.example.listaelementos.database.ListaComprasDatabase
 import com.example.listaelementos.repositories.ProdutoRepository
+import com.example.listaelementos.ui.viewmodels.MainViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
-val storageModule = module {
+val appModule = module {
     singleOf(::ProdutoRepository)
+    viewModelOf(::MainViewModel)
     single {
         Room.databaseBuilder(
             androidContext(),
-            ListaComprasDatabase::class.java, "lista_compras.db"
+            ListaComprasDatabase::class.java, "lista_compras_database"
         ).build()
     }
     single {
