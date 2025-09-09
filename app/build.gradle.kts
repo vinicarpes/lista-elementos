dd4210469981b7b45c4077e26a2056642a98d236
vinicarpes <vinicdf000@gmail.com>
2025-09-09 11:36:57 -0300

    build: add missing dependency for compose preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.activity.compose)
 }
