a22d0184b5f9188b22c84efd81308cce25ee5a60
vinicarpes <vinicdf000@gmail.com>
2025-09-25 10:41:59 -0300

    feat: setup Koin for new view and viewModel

--- app/src/main/java/com/example/listaelementos/di/KoinModule.kt
+++ app/src/main/java/com/example/listaelementos/di/KoinModule.kt
@@ -3,14 +3,17 @@ package com.example.listaelementos.di
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.room.Room
 import com.example.listaelementos.database.ListaComprasDatabase
+import com.example.listaelementos.repositories.LojaRepository
 import com.example.listaelementos.repositories.ProdutoRepository
 import com.example.listaelementos.retrofit.provideOkHttpClient
 import com.example.listaelementos.retrofit.provideRetrofit
 import com.example.listaelementos.retrofit.service.ApiService
 import com.example.listaelementos.ui.viewmodels.CadastroComposeViewModel
 import com.example.listaelementos.ui.viewmodels.CadastroViewModel
+import com.example.listaelementos.ui.viewmodels.ListaLojasComposeViewModel
 import com.example.listaelementos.ui.viewmodels.MainViewModel
 import com.example.listaelementos.ui.viewmodels.MainComposeViewModel
+import com.example.listaelementos.usecases.LojaUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
@@ -23,6 +26,7 @@ val appModule = module {
     viewModelOf(::CadastroViewModel)
     viewModelOf(::MainComposeViewModel)
     viewModelOf(::CadastroComposeViewModel)
+    viewModelOf(::ListaLojasComposeViewModel)
 
     single {
         Room.databaseBuilder(
@@ -39,4 +43,6 @@ val networkModule = module {
     single { provideOkHttpClient(true) }
     single { provideRetrofit(get()) }
     single < ApiService> {get<Retrofit>().create(ApiService::class.java)}
+    single { LojaRepository(get()) }
+    single { LojaUseCase(get()) }
 }
