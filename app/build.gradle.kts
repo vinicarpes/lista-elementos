bf5623afd641a6b30d5c2d7d8d2be42e08067ee1
vinicarpes <vinicdf000@gmail.com>
2025-09-29 14:27:58 -0300

    build: add firebase authentication dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,5 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 }
