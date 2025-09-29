000dd20f76b115d6ed47a0f23feeba8748096434
vinicarpes <vinicdf000@gmail.com>
2025-09-29 10:02:02 -0300

    style: change onSurface color and remove unused icon

--- app/src/main/java/com/example/listaelementos/ui/components/ListaComprasTopBar.kt
+++ app/src/main/java/com/example/listaelementos/ui/components/ListaComprasTopBar.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import com.example.listaelementos.ui.theme.md_theme_dark_onPrimary
 import com.example.listaelementos.ui.theme.md_theme_dark_primaryContainer
@@ -37,14 +38,6 @@ fun ListaComprasTopBar(context: AppCompatActivity) {
             IconButton(onClick = { context.finish() }) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "Localized description"
-                )
-            }
-        },
-        actions = {
-            IconButton(onClick = { /* do something */ }) {
-                Icon(
-                    imageVector = Icons.Filled.Menu,
                     contentDescription = "Localized description"
                 )
             }
